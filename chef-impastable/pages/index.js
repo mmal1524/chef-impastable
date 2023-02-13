4a58627dbb2640ace26216158a5101f54861943c
Mahima <mmalhotra017@gmail.com>
2023-02-13 13:47:35 -0500

    merge next setup with mongo setup

--- chef-impastable/pages/index.js
+++ chef-impastable/pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
 import { InferGetServerSidePropsType } from 'next'
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export async function getServerSideProps(context) {
   try {
@@ -48,41 +51,16 @@ export default function Home({
             for instructions.
           </h2>
         )}
-
-        <p className="description">
+        <h1 className={styles.title}>
+          Welcome to Chef Impastable!
+        </h1>
+        <Image src={"/chef-impastable-logo.jpg"} width={300} height={150} />
+        <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.grid}>
+            Lets go to a <Link href="/second-post"> new page!</Link>
         </div>
       </main>
 

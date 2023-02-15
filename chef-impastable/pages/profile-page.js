d89de52170f8b9ce3a9b931bf7d95493fd795acc
sawagler <sawagler@purdue.edu>
2023-02-15 13:11:38 -0500

    See profile avatar, name, and username

--- chef-impastable/pages/profile-page.js
+++ chef-impastable/pages/profile-page.js
@@ -1,9 +1,44 @@
+import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
 
 export default function ProfilePage() {
     return (
         <>
-            <h1>Profile</h1>
+            <div>
+                <Stack
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="baseline"
+                    spacing={3}
+                >
+                    <Avatar sx={{width: 85, height: 85}}>SW</Avatar>
+
+                    <Stack
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="stretch"
+                        spacing={0}
+                    >
+
+                        <h1 className="h1">Sarah Wagler</h1>
+                        <h3 className="h3">sawagler</h3>
+
+                    </Stack>
+                </Stack>
+
+                <style jsx>{`
+                    .h1 {
+                        margin: 0px;
+                    }
+                    .h3 {
+                        margin: 0px;
+                    }
+                `}</style>
+
+            </div>
+            
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>

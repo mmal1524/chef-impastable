44c330ca0e41e310f4772342336c4d5d7ae7be17
sawagler <sawagler@purdue.edu>
2023-02-15 19:19:18 -0500

    Created a grid to put a button for editing profile

--- chef-impastable/pages/profile-page.js
+++ chef-impastable/pages/profile-page.js
@@ -2,6 +2,9 @@ import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Unstable_Grid2';
+import Button from '@mui/material/Button';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export default function ProfilePage() {
 
@@ -9,13 +12,6 @@ export default function ProfilePage() {
         displayName: 'Sarah Wagler',
         username: 'sawagler',
         profilePicture: "",
-    }
-
-    function imageExists(url) {
-        const img = new Image();
-        img.src = url;
-        if (img.complete) { return true; }
-        return false;
     }
 
     function getInitials(name) {
@@ -31,34 +27,43 @@ export default function ProfilePage() {
     return (
         <>
             <div>
-                <Stack
-                    direction="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                    spacing={3}
-                >
-                    
-                    <Avatar 
-                        sx={{width: 85, height: 85}} 
-                        alt={user.displayName} 
-                        src={user.profilePicture} 
-                    >
-                        {getInitials(user.displayName)}
-                    </Avatar>
 
-                    <Stack
-                        direction="column"
-                        justifyContent="center"
-                        alignItems="stretch"
-                        spacing={0}
-                    >
+                <Grid container spacing={1}>
+                    <Grid xs>
+                        {/* Stack to display profile picture, display name, and username */}
+                        <Stack
+                            direction="row"
+                            justifyContent="flex-start"
+                            alignItems="center"
+                            spacing={3}
+                        >
+                            {/* Profile Picture, if no url, displays user's initials */}
+                            <Avatar 
+                                sx={{width: 85, height: 85}} 
+                                alt={user.displayName} 
+                                src={user.profilePicture} 
+                            >
+                                {getInitials(user.displayName)}
+                            </Avatar>
+                            {/* Displays user's display name and username next to profile picture */}
+                            <Stack
+                                direction="column"
+                                justifyContent="center"
+                                alignItems="stretch"
+                                spacing={0}
+                            >
 
-                        <h1 className="h1">{user.displayName}</h1>
-                        <h3 className="h3">{user.username}</h3>
-
-                    </Stack>
-                    
-                </Stack>
+                                <h1 className="h1">{user.displayName}</h1>
+                                <h3 className="h3">{user.username}</h3>
+                            </Stack>
+                        </Stack>
+                    </Grid>
+                    <Grid xs={2}>
+                        <Button variant="outlined" startIcon={<SettingsIcon />}>
+                            Edit Profile
+                        </Button>
+                    </Grid>
+                </Grid>
 
                 <style jsx>{`
                     .h1 {

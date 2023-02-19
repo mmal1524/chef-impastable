308a855d1463fc3febc6d3ff882cefe46aee5e27
sawagler <sawagler@purdue.edu>
2023-02-18 23:08:51 -0500

    Switched user class to its own component file

---
+++ chef-impastable/components/user.js
@@ -0,0 +1,116 @@
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+
+export default class User {
+    constructor(displayName, username, profilePicture, friends, friendRequests) {
+        this.displayName = displayName;
+        this.username = username;
+        this.profilePicture = profilePicture;
+        this.friends = friends;
+        this.friendRequests = friendRequests;
+    }
+
+    getInitials() {
+        var words = this.displayName.split(' ');
+        var initials = "";
+        words.forEach(function(word) {
+            initials += word[0];
+        });
+        return initials;
+    }
+
+    displayLarge() {
+        return (
+            <>
+                {/* Stack to display user's profile picture, display name, and username */}
+                <Stack
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="center"
+                    spacing={3}
+                >
+                    {/* Profile Picture, if no url, displays user's initials */}
+                    <Avatar 
+                        sx={{width: 85, height: 85}} 
+                        alt={this.displayName} 
+                        src={this.profilePicture} 
+                    >
+                        {this.getInitials()}
+                    </Avatar>
+                    {/* Displays user's display name and username next to profile picture */}
+                    <Stack
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="stretch"
+                        spacing={0}
+                    >
+                        <h1 className="displayName">{this.displayName}</h1>
+                        <h3 className="username">{this.username}</h3>
+                    </Stack>
+
+                </Stack>
+
+                <style jsx>{`
+                    .displayName {
+                        margin: 0px;
+                    }
+                    .username {
+                        margin: 0px;
+                    }
+                `}</style>
+            </>
+
+        );
+    }
+
+    displaySmall() {
+        return (
+            <Box sx={{margin: 1, marginLeft: 0}}>
+                {/* Stack to display user's profile picture, display name, and username */}
+                <Stack
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="center"
+                    spacing={3}
+                >
+                    {/* Profile Picture, if no url, displays user's initials */}
+                    <Avatar 
+                        sx={{width: 40, height: 40}} 
+                        alt={this.displayName} 
+                        src={this.profilePicture} 
+                    >
+                        {this.getInitials()}
+                    </Avatar>
+                    {/* Displays user's display name and username next to profile picture */}
+                    <Stack
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="stretch"
+                        spacing={0}
+                    >
+                        <h3 className="displayName">{this.displayName}</h3>
+                        <h5 className="username">{this.username}</h5>
+                    </Stack>
+
+                </Stack>
+
+                <style jsx>{`
+                    .displayName {
+                        margin: 0px;
+                    }
+                    .username {
+                        margin: 0px;
+                    }
+                `}</style>
+            </Box>
+
+        );
+    }
+
+    displayFriends() {
+        for (let i = 0; i < this.friends.length; i++) {
+            this.friends[i].displaySmall();
+        }
+    }
+}
--- chef-impastable/pages/profile-page.js
+++ chef-impastable/pages/profile-page.js
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import styles from '../styles/Home.module.css';
+import User from '../components/user';
 import Link from 'next/link';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Unstable_Grid2';
 import Button from '@mui/material/Button';
 import SettingsIcon from '@mui/icons-material/Settings';
@@ -45,120 +44,6 @@ function a11yProps(index) {
       'aria-controls': `simple-tabpanel-${index}`,
     };
 }
-
-class User {
-    constructor(displayName, username, profilePicture, friends, friendRequests) {
-        this.displayName = displayName;
-        this.username = username;
-        this.profilePicture = profilePicture;
-        this.friends = friends;
-        this.friendRequests = friendRequests;
-    }
-
-    getInitials() {
-        var words = this.displayName.split(' ');
-        var initials = "";
-        words.forEach(function(word) {
-            initials += word[0];
-        });
-        return initials;
-    }
-
-    displayLarge() {
-        return (
-            <>
-                {/* Stack to display user's profile picture, display name, and username */}
-                <Stack
-                    direction="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                    spacing={3}
-                >
-                    {/* Profile Picture, if no url, displays user's initials */}
-                    <Avatar 
-                        sx={{width: 85, height: 85}} 
-                        alt={this.displayName} 
-                        src={this.profilePicture} 
-                    >
-                        {this.getInitials()}
-                    </Avatar>
-                    {/* Displays user's display name and username next to profile picture */}
-                    <Stack
-                        direction="column"
-                        justifyContent="center"
-                        alignItems="stretch"
-                        spacing={0}
-                    >
-                        <h1 className="displayName">{this.displayName}</h1>
-                        <h3 className="username">{this.username}</h3>
-                    </Stack>
-
-                </Stack>
-
-                <style jsx>{`
-                    .displayName {
-                        margin: 0px;
-                    }
-                    .username {
-                        margin: 0px;
-                    }
-                `}</style>
-            </>
-
-        );
-    }
-
-    displaySmall() {
-        return (
-            <Box sx={{margin: 1, marginLeft: 0}}>
-                {/* Stack to display user's profile picture, display name, and username */}
-                <Stack
-                    direction="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                    spacing={3}
-                >
-                    {/* Profile Picture, if no url, displays user's initials */}
-                    <Avatar 
-                        sx={{width: 40, height: 40}} 
-                        alt={this.displayName} 
-                        src={this.profilePicture} 
-                    >
-                        {this.getInitials()}
-                    </Avatar>
-                    {/* Displays user's display name and username next to profile picture */}
-                    <Stack
-                        direction="column"
-                        justifyContent="center"
-                        alignItems="stretch"
-                        spacing={0}
-                    >
-                        <h3 className="displayName">{this.displayName}</h3>
-                        <h5 className="username">{this.username}</h5>
-                    </Stack>
-
-                </Stack>
-
-                <style jsx>{`
-                    .displayName {
-                        margin: 0px;
-                    }
-                    .username {
-                        margin: 0px;
-                    }
-                `}</style>
-            </Box>
-
-        );
-    }
-
-    displayFriends() {
-        for (let i = 0; i < this.friends.length; i++) {
-            this.friends[i].displaySmall();
-        }
-    }
-}
-
 
 export default function ProfilePage() {
 

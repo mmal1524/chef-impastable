cb820c7a9bd19b4c1249aa95d2f020dbd973e8a7
Carmen Del Mastro <carmendiem2003@gmail.com>
2023-02-22 13:04:28 -0500

    Added UI for logout confirmation and logout routes

--- chef-impastable/pages/navbar.js
+++ chef-impastable/pages/navbar.js
@@ -5,10 +5,18 @@ import User from '../components/user';
 import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { useRouter } from "next/router";
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Dialog } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 const Navbar = () => {
 
-    var user = new User('Sarah Wagler', 'sawagler', "",[],[]);
+    var user = new User('Sarah Wagler', 'sawagler', "", [], []);
 
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
@@ -19,56 +27,89 @@ const Navbar = () => {
         setAnchorEl(null);
     };
 
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const logout = () => {
+        router.push("/");
+    }
+    const router = useRouter();
+
     return (
-    <Grid container spacing={0} sx={{width: '100vw', border: 4, borderColor: 'Orange'}}>
-        <Grid xs={11}>
-            Navbar
-        </Grid>
-        <Grid xs={1}>
-            <Button 
-                variant="text" 
-                sx={{color: 'black'}}
-                aria-controls={open ? 'navbar' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-            >
-                    <Avatar 
-                        sx={{width: 40, height: 40}} 
-                        alt={user.displayName} 
-                        src={user.profilePicture} 
+        <Grid container spacing={0} sx={{ width: '100vw', border: 4, borderColor: 'Orange' }}>
+            <Grid xs={11}>
+                Navbar
+            </Grid>
+            <Grid xs={1}>
+                <Button
+                    variant="text"
+                    sx={{ color: 'black' }}
+                    aria-controls={open ? 'navbar' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                >
+                    <Avatar
+                        sx={{ width: 40, height: 40 }}
+                        alt={user.displayName}
+                        src={user.profilePicture}
                     >
                         {user.getInitials()}
                     </Avatar>
-            </Button>
-            <Menu
-                aria-labelledby="navbar"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-                }}
-                transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-                }}
-            >
-                <MenuItem 
-                    onClick={() => {
-                        window.location.href="/profile-page";
+                </Button>
+                <Menu
+                    aria-labelledby="navbar"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
                     }}
                 >
-                    Profile
-                </MenuItem>
-                <MenuItem 
-                >
-                    Logout
-                </MenuItem>
-            </Menu>
+                    <MenuItem
+                        onClick={() => {
+                            window.location.href = "/profile-page";
+                        }}
+                    >
+                        Profile
+                    </MenuItem>
+                    <MenuItem
+                        onClick={(handleClickOpen) => {
+                        }}
+                    >
+                        Logout
+
+                    </MenuItem>
+                </Menu>
+            </Grid>
+            <Dialog
+                fullScreen={fullScreen}
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="responsive-dialog-title"
+            >
+                <DialogTitle id="responsive-dialog-title">
+                    {"Logout Confirmation"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to log out?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={logout} autoFocus>
+                        Yes
+                    </Button>
+                    <Button onClick={handleClose} autoFocus>
+                        No
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Grid>
-    </Grid>
     );
 }
 

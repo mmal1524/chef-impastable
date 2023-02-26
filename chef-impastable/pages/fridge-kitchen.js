20df14a50952aa270ecad253a895bd96ee47b585
jiahui <huan1681@purdue.edu>
2023-02-25 23:20:33 -0500

    make separate fridge and kitchen tabs

--- chef-impastable/pages/fridge-kitchen.js
+++ chef-impastable/pages/fridge-kitchen.js
@@ -1,12 +1,77 @@
+import * as React from 'react';
 import Link from 'next/link';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+import { Divider } from '@mui/material';
+import Navbar from './navbar.js'
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
 
 export default function Kitchen() {
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <>
             <h1>This will be the Fridge/Kitchen page</h1>
             <h2>
                 <Link href="/homepage">Back to home</Link>
             </h2>
+            <div>
+                <Box sx={{ width: '100%' }}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <Tabs value={value} onChange={handleChange}>
+                            <Tab label="Fridge" {...a11yProps(0)} />
+                            <Tab label="Kitchen" {...a11yProps(1)} />
+                        </Tabs>
+                    </Box>
+                    <TabPanel value={value} index={0}>
+                        Fridge
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        Kitchen
+                    </TabPanel>
+                </Box>
+            </div>
         </>
     );
 }

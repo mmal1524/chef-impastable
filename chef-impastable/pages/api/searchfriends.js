642d5a178e4ba6bd4c95047fb05c24680054a3e9
kfruehau <kfruehau@purdue.edu>
2023-03-01 00:17:54 -0500

    almost have the searching for friends figured out, running into a couple of errors

---
+++ chef-impastable/pages/api/searchfriends.js
@@ -0,0 +1,33 @@
+import connect from "../../lib/mongodb"
+import User from "../../model/schema"
+
+let mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
+connect()
+
+export default async function handler(req,res){
+    try {
+        const {username}=req.body
+        const user = await User.findOne({username})
+        if (!user) {
+            return null;
+        }
+        else {
+            return res.json({
+                username: user.username,
+                password: user.password,
+                displayName: user.displayName,
+                avatar: user.avatar,
+                friends: user.friends,
+                friendRequests: user.friendRequests,
+                createdPrivacy: user.createdPrivacy,
+                savedPrivacy: user.savedPrivacy,
+                reviewedPrivacy: user.reviewedPrivacy,
+                dietaryTags: user.dietaryTags
+            });
+        }
+    } catch (error) {
+        res.status(400).json({status:'Not able to create a new user.'})
+        console.log('error');
+    }
+}
--- chef-impastable/pages/friends.js
+++ chef-impastable/pages/friends.js
@@ -2,6 +2,7 @@ import Navbar from './navbar.js'
 import * as React from 'react';
 import User from '../components/user';
 import { currUser } from '../components/user';
+import { useState, useEffect } from "react";
 import Link from 'next/link';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
@@ -19,13 +20,50 @@ import SearchIcon from '@mui/icons-material/Search';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import { useRef } from 'react';
 
-export default function FriendsPage() {
+//const FriendSearch = ({ friends }) => {
+//  const [searchQuery, setSearchQuery] = useState("");
+
+//  const filteredFriends = friends.filter((friend) =>
+//    friend.name.toLowerCase().includes(searchQuery.toLowerCase())
+ // );
+
+
+export default function FriendsPage({users}) {
+    //console.log(users)
+
     var friend1 = new User('Mahima', 'mahimasusername', "", [], []);
     var friend2 = new User('Jiahui', 'jiahuisusername', "", [], []);
     var friend3 = new User('Kendalyn', 'kendalynsusername', "", [], []);
     var friend4 = new User('Carmen', 'carmentsusername', "", [], []);
     var user = new User('Sarah Wagler', 'sawagler', "", [friend1, friend2, friend3], [friend4]);
+
+    const [usernameValue, setUsernameValue] = useState("");
+    const [allUsers, setAllUsers] = useState([]);
+    const [query, setQuery] = useState("")
+    const [displayU, setDisplayU] = useState(false);
+    const [searchUser, setSearchUser] = useState(null);
+    // don't need a router, need to have a list of users come up
+
+    const sendValue = async (e) => {
+      e.preventDefault();
+      const response = await fetch('/api/searchfriends');
+      const data = await response.json();
+      setAllUsers(data);
+      return console.log("hi")
+      return console.log(usernameValue.target.value);
+    }
+
+    const handleGetUserName = e => {
+      setUsernameValue(e.target.value)
+    }
+
+    const handleNoUser = e => {
+      setDisplayU(false);
+    }
+
+    console.log(usernameValue)
 
     return (
         <div>
@@ -37,12 +75,13 @@ export default function FriendsPage() {
                 <h3 className="h3">Search For Friends</h3>
                 <Divider />
                 <TextField
-                    error
-                    id="standard-error-helper-text"
-                    label="Error"
-                    defaultValue="Enter Username"
-                    helperText="No users found"
+                    id="outlined-search"
+                    label=""
+                    defaultValue="Search..."
+                    helperText=""
                     variant="standard"
+                    value={usernameValue}
+                    onChange={handleGetUserName}
                     InputProps={{
                         startAdornment: (
                           <InputAdornment position="start">
@@ -51,6 +90,37 @@ export default function FriendsPage() {
                         ),
                       }}
                     />
+                    <Button
+                      variant='contained'
+                      color='primary'
+                      size='small'
+                      onClick={ async () => {
+                        var foundUser = await findUser(usernameValue);
+                        if (!foundUser.success) {
+                          console.log("no")
+                        } else {
+                          console.log("yes")
+                        }
+                        //<Grid container spacing={3}>
+                        //{user.map((usernameValue) => (
+                            //<Grid> item key={user.id}>
+                           //</Grid>   <UserDisplay = user={user}/>
+                           // </Grid>
+                         //</Grid> ))}
+                        //</Grid>
+                       // if (foundUser.success) {
+
+                        //  handleGetUser();
+                        //} else {
+                        //  <div>
+                          //</div>  <p> hello </p>
+                          //</div>
+                          //handleNoUser(); 
+                        //}
+                      }}
+                      >
+                    Search
+                    </Button>
                     {<Grid container spacing={4} direction = "row">
                             {user.friends[0].displaySmall()}
                             {<Button variant="outlined" endIcon={<SendIcon />}>Request</Button>}
@@ -82,4 +152,22 @@ export default function FriendsPage() {
             </Grid>
         </div>
     );
+
+  async function findUser(username) {
+    const res = await fetch('/api/searchfriends', {
+      method: 'POST', 
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+      })
+    })
+    console.log("at the end of findUser")
+    const data = await res.json();
+    console.log(data)
+    return data;
+  } 
 }
+

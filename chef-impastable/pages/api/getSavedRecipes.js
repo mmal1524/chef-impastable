f12975fe6e7026da1c66886826a213f85ddcd550
Mahima <mmalhotra017@gmail.com>
2023-03-19 15:05:04 -0400

    update routing to optionally pull saved recipe data

--- chef-impastable/pages/api/getSavedRecipes.js
+++ chef-impastable/pages/api/getSavedRecipes.js
@@ -1,5 +1,6 @@
 import connect from "../../lib/mongodb"
 import SavedFolder from "../../model/savedFolder";
+import Recipe from "../../model/recipe";
 
 let mongoose = require('mongoose')
 mongoose.set('strictQuery', false);
@@ -8,12 +9,22 @@ connect()
 export default async function getSavedFolders(req,res){
     try {
         const username=req.body.user;
+        const getRecipeData = req.body.getData;
         const folders = await SavedFolder.find({user: username});
         console.log(folders);
         if (!folders) {
             res.status(400).json({status: "Not able to find folders"});
         }
         else {
+            if (getRecipeData) {
+                folders.forEach( (folder) => {
+                    folder.recipes = folder.recipes.map( async (recipeID) => {
+                        await Recipe.findById(recipeID);
+                    });
+                    console.log(folder.recipes);
+                });
+                console.log(folders);
+            }
             return res.json(folders);
         }
     } catch (error) {

e2227205be74d10c8981c9fa1a1e725e16441533
sawagler <sawagler@purdue.edu>
2023-03-28 17:38:58 -0400

    Fixed cypress refreshing session data

--- chef-impastable/cypress/e2e/reviews.cy.ts
+++ chef-impastable/cypress/e2e/reviews.cy.ts
@@ -1,22 +1,24 @@
 import React from "react"
 
-it('Review Page', () => {
+describe('Reviews', () => {
 
-  describe("Logging in", () => {
+  it("Logging in", () => {
     cy.visit('/')
     cy.get("[data-test='UsernameField']").type("reviewedrecipes")
     cy.get("[data-test='PasswordField']").type("Password1!")
     cy.get("[data-test='LoginButton']").click()
   })
 
-  describe("Clicking a recipe", () => {
-    cy.get("[data-test='Recipe-0']", {timeout: 15000}).click()
+  it("Clicking a recipe", () => {
+    cy.get("[data-test='Recipe-6']", {timeout: 60000}).click()
   })
 
-  describe('Leave a review', () => {
-    cy.get("[data-test='LeaveAReview']", {timeout: 15000}).should("exist").should("contain", "Leave a Review").click()
+  it("Leave a review button", () => {
+    cy.get("[data-test='LeaveAReview']", {timeout: 60000}).should("contain", "Leave a Review").click()
+  })
 
-    cy.get("[role='dialog']").should("exist").should("contain", "Leave a Review")
+  it("UI dialog popup", () => {
+    cy.get("[role='dialog']").should("contain", "Leave a Review")
 
     cy.get("[data-test='ReviewUI']")
       .should("contain", "Rating")
@@ -30,6 +32,7 @@ it('Review Page', () => {
       .get("[data-test='Star5']").should("exist")
 
     cy.get("[data-test='description']").should("exist")
-
   })
+
+  
 })

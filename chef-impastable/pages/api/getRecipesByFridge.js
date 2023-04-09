d4ae67199e7e352c02414507cda511e9ff2b8872
Mahima <mmalhotra017@gmail.com>
2023-04-08 23:24:31 -0400

    filter attempt

--- chef-impastable/pages/api/getRecipesByFridge.js
+++ chef-impastable/pages/api/getRecipesByFridge.js
@@ -13,6 +13,22 @@ export default async function getRecipesByFridge(req,res){
         // check if the ingredent is in the user's fridge
         // count how many times the each recipes has a match in the fridge
         // sort by match count
+        const user = await User.findOne({username})
+        
+        var data = await Recipe.aggregate([
+            {
+                $project: {
+                    matches: {
+                        $filter: {
+                            input: "$ingredients",
+                            as: "ingredient",
+                            cond: { $in: ["$$item", user.fridge]}
+                        }
+                    }
+                }
+            }
+        ])
+        /*
         var data = await Recipe.find({
             $expr: {$function: {
                 body: function() {
@@ -23,7 +39,7 @@ export default async function getRecipesByFridge(req,res){
                 lang: "js"
             }}
         });
-                 
+              */   
     } catch (error) {
         res.status(400).json({status:'Not able to find folders'});
         console.log('error');

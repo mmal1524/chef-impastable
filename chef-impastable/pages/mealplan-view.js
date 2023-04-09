4d89b1b87777242481f0ac5f06266633faa8104b
sawagler <sawagler@purdue.edu>
2023-04-09 12:03:38 -0400

    Routes for grabbing meal plans

--- chef-impastable/pages/mealplan-view.js
+++ chef-impastable/pages/mealplan-view.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import clientPromise from '../lib/mongodb_client';
 import Navbar from './navbar';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
@@ -29,4 +30,37 @@ export default function MealPlan() {
             </div>
         </>
     );
+}
+
+export async function getServerSideProps(context) {
+    try {
+
+        const client = await clientPromise;
+        const db = client.db("test");
+
+        const user = await db
+            .collection("users")
+            .findOne({username: context.query.username});
+
+        var mealPlanNames = user.mealPlans;
+
+        var mealPlans = new Array(mealPlanNames.length);
+        var i = 0;
+        for (i; i < mealPlanNames.length; i++) {
+            var mealPlan = await db
+                .collection("mealplans")
+                .findOne({name: mealPlanNames[i], user: context.query.username})
+            mealPlans[i] = JSON.parse(JSON.stringify(mealPlan))
+        }
+
+        var recipes = new Array(mealPlanNames.length);
+        for (i = 0; i < mealPlanNames.length; i++) {
+            recipes[i] = new Array(7);
+        }
+
+
+    }
+    catch (e) {
+        console.error(e);
+    }
 }

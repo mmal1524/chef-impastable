67d9ed90f8d0919f41a2b425e902784f4fd9ff54
sawagler <sawagler@purdue.edu>
2023-04-11 01:42:09 -0400

    Routes and displays meal plans 4 hours

--- chef-impastable/pages/mealplan-view.js
+++ chef-impastable/pages/mealplan-view.js
@@ -1,18 +1,151 @@
 import * as React from 'react';
 import clientPromise from '../lib/mongodb_client';
 import Navbar from './navbar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PropTypes from 'prop-types';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import { ListItem } from '@mui/material';
 import { ObjectId } from 'mongodb';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
+import { red, blue } from '@mui/material/colors';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3, width: '100%'}}>
+            <Box sx={{width: '100%'}}>{children}</Box>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+  }
 
 export default function MealPlan({mealPlans, recipes}) {
+
+    const [tabs, setTabs] = React.useState(0);
+
+    const handleChangeTabs = (event, newValue) => {
+        setTabs(newValue);
+    };
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
     return (
         <>
             <div className="App">
                     <Navbar />
             </div>
-            <div>
+            <Box
+                sx={{display: 'flex', width: '100%'}}
+            >
+                <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={tabs}
+                    onChange={handleChangeTabs}
+                    aria-label="Vertical tabs example"
+                    sx={{ borderRight: 1, borderColor: 'divider', width: 150 }}
+                >
+                    {mealPlans.map((mealPlan, index) => (
+                        <Tab label={mealPlan.name} {...a11yProps(index)}/>
+                    ))}
+                </Tabs>
+                <Box sx={{width: '100%'}}>
+                    {mealPlans.map((mealPlan, index) => (
+                        <TabPanel value={tabs} index={index} >
+                            <Grid 
+                                container 
+                                spacing={0}
+                                columns={8} 
+                                direction="row"
+                                justifyContent="flex-start"
+                                alignItems="stretch"
+                            >
+                                {daysOfWeek.map((day, i) => (
+                                    <>
+                                        <Grid item xs={2} sx={{border: 1, padding: 1}}>
+                                            <Box sx={{borderBottom: 1, borderColor: 'black'}}>
+                                                <h4 align="center">{day}</h4>
+                                            </Box>
+                                            {recipes.at(index).at(i).map((recipe) => (
+                                                <div>
+                                                    <p className='name'>{recipe.title}</p>
+                                                    <Divider textAlign='right'>
+                                                        <IconButton sx={{padding: 1}}>
+                                                            <DeleteIcon sx={{fontSize: 20, color: red[200]}}/>
+                                                        </IconButton>
+                                                        <IconButton sx={{padding: 1}}>
+                                                            <SyncAltIcon sx={{fontSize: 20, color: blue[300]}}/>
+                                                        </IconButton>
+                                                    </Divider>
+                                                    <style jsx>{`
+                                                        .name {
+                                                            margin-bottom: 0px;
+                                                        }
+                                                    `}</style>
+                                                </div>
+                                            ))}
+                                        </Grid>
+                                    </>
+                                ))}
+                                
+                            </Grid>
+                            
+                        </TabPanel>
+                    ))}
+                </Box>
+                {/* <TabPanel value={tabs} index={0}>
+                    Item One
+                </TabPanel>
+                <TabPanel value={tabs} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={tabs} index={2}>
+                    Item Three
+                </TabPanel>
+                <TabPanel value={tabs} index={3}>
+                    Item Four
+                </TabPanel>
+                <TabPanel value={tabs} index={4}>
+                    Item Five
+                </TabPanel>
+                <TabPanel value={tabs} index={5}>
+                    Item Six
+                </TabPanel>
+                <TabPanel value={tabs} index={6}>
+                    Item Seven
+                </TabPanel> */}
+            </Box>
+  
+            {/* <div>
                 <Stack
                     direction="row"
                     divider={<Divider orientation="vertical" flexItem />}
@@ -28,9 +161,10 @@ export default function MealPlan({mealPlans, recipes}) {
                     <h4>Friday</h4>
                     <h4>Saturday</h4>
                 </Stack>
-            </div>
+            </div> */}
         </>
     );
+
 }
 
 export async function getServerSideProps(context) {
